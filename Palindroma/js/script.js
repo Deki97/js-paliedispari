0bc0fd17ea7b2036be23b8dc2fc3a01ac351c078
Deki97 <dejan97@libero.it>
2021-11-22 16:01:59 +0100

    done

--- Palindroma/js/script.js
+++ Palindroma/js/script.js
@@ -3,3 +3,35 @@
 // Creare una funzione per capire se la parola inserita è palindroma
 
 
+// Chiedo all'utente di inserire una parola
+const userWord = prompt('Inserisci una parola');
+console.log(userWord);
+
+// Creo una variabile che contiene il richiamo alla funzione, che conterrà al suo interno il risultato (true o false)
+let userWordIsPalindrome = isPalindrome(userWord);
+console.log(userWordIsPalindrome);
+
+
+// Verifico che il risultato della funzione è true, in quel caso stampo che la parola è palindroma, altrimenti non è palindroma
+if(userWordIsPalindrome == true) {
+    alert('La parola è palindroma');
+} else {
+    alert('La parola non è palindroma');
+}
+
+// FUNZIONE
+function isPalindrome(word) {
+    let parola = '';
+    let isPali = false;
+    for(let i = word.length - 1; i >= 0; i--) {
+        let thisLetter = word[i];
+        parola += thisLetter; 
+    }
+    console.log(parola);
+
+    if(parola === word) {
+        isPali = true;
+    }
+
+    return isPali;
+}
